e484713de3692408e54729fa2b7d28f1ac944e87
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2022-02-24 09:34:15 +0200

    Add new scriptlet: removeNonFreeEntries.js

--- custom_scriptlets.js
+++ custom_scriptlets.js
@@ -2,3 +2,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('.latest_shelf')[1].scrollIntoView();
 });
+
+/// removeNonFreeEntries.js
+window.addEventListener('DOMContentLoaded', (event) => {
+    siteTableElement = document.querySelector('#siteTable');
+    for (let node of siteTableElement.childNodes) {
+        if (node.className.split(/\s/).includes('link')) {
+            titleText = node.childNodes[4].firstChild.firstChild.firstChild.innerHTML;
+            if (!titleText.includes('FREE') && !titleText.includes('100') && !titleText.includes('Free')) {
+                node.remove();
+            }
+        }
+    }
+});
